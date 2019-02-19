cfac870b9bc443f7019c18e063a7be2a72439c68
SukachevAlex <sukachevalexandr@gmail.com>
2019-02-19 22:51:45 +0300

    Change text-area
    Try to make text-area fine, change margins for pixel-perfect

--- app/scripts/textarea.js
+++ app/scripts/textarea.js
@@ -1,7 +1,13 @@
 export default function initTextarea() {
 	const textarea = document.querySelector('.input-textarea');
-	const textareaMinRows = 18;
-	const textareaMaxRows = 25;
+	const textareaMinRows = 3;
+	const textareaMaxRows = 20;
+
+	textarea.value = textarea.value
+		.replace(/\n/g, '')
+		.replace(/\s+/g, ' ')
+		.split('.').join('.\n');
+	textarea.setAttribute('rows', textarea.value.split('\n').length);
 
 	textarea.addEventListener('keyup', function () {
 		const rowCount = textarea.value.split('\n').length;
